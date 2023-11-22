1890a5154699dbba499eb2f86be1d592b06d2040
capa026 <118733637+capa026@users.noreply.github.com>
2023-11-22 02:07:31 +0000

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-function App() {
+import styled from "@emotion/styled";
+import { Settings } from "@mui/icons-material";
+import { Button } from "@mui/material";
 
+function App() {
   return (
-    <div className="a">HOLA REACT</div>
-  )
+    <div>
+      <Button
+        startIcon={<Settings />}
+        variant="contained"
+        color="primary"
+        sx={{
+          "&:hover": {
+            backgroundColor: "#000",
+          },
+        }}
+      >
+        Create
+      </Button>
+
+      <Button variant="contained" color="secondary">
+        New btn
+      </Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/utils/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "purple",
+    },
+    secondary: {
+      main: "tomato",
+    },
+    background: {
+      main: "#212121",
+    },
+  },
+});
