6053b4652ae1f7f7e34ddf2b90477b475d5f2aeb
Carlos Pacheco <carlospacheco.fx@gmail.com>
2023-12-01 19:57:18 -0400

    Navbar mod

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = () => {
     <AppBar position="sticky" color="backgroundDark" sx={{ color: "white" }}>
       <StyledToolbar>
         <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
-          New App
+          New Appp
         </Typography>
         <AccountTree sx={{ display: { xs: "block", sm: "none" } }} />
         <Search>
